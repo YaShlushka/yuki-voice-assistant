94e953135864fc8bcf5847ad2d05fb90634dd99e
YaShlushka <paderinee@gmail.com>
2025-09-28 14:50:17 +0300

    Added recognition of whether someone is speaking into the microphone

--- main.cpp
+++ main.cpp
@@ -1,14 +1,27 @@
 #include "miniaudio.h"
 #include <iostream>
 #include <vector>
+#include <cstdint>
 
 std::vector<int16_t> audio_buffer;
+uint16_t last_speak_time = 0;
+bool is_speak = false;
 
-void data_callback(ma_device* pDevice, void* pOutput, const void* pInput, ma_uint32 frameCount) {
+void data_callback(ma_device* pDevice, void* pOutput, const void* pInput, ma_uint32 frame_count) {
 	const int16_t* input_samples = (const int16_t*)pInput;
-	for (ma_uint32 i = 0; i < frameCount; i++) {
+
+	for (ma_uint32 i = 0; i < frame_count; i++) {
 		audio_buffer.push_back(input_samples[i]);
+
+		last_speak_time = input_samples[i] > 1000 ? 0 : (last_speak_time > 5000 ? last_speak_time : last_speak_time + 1);
+		is_speak = last_speak_time < 5001 ? true : false;
 	}
+
+	if(!is_speak) {
+		audio_buffer.clear();
+	}
+
+	std::cout << audio_buffer.size() << std::endl;
 }
 
 int main() {
