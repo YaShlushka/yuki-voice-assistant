c9c5b679e725d434c09b50bd99b252602af728b3
YaShlushka <paderinee@gmail.com>
2025-12-15 21:31:20 +0300

    Deleted phrase_recognizer.h/cpp

--- src/phrase_recognizer.cpp
+++
@@ -1,5 +0,0 @@
-#include "phrase_recognizer.h"
-
-std::string RecognizePhrase(const std::string& str) {
-
-}
--- src/phrase_recognizer.h
+++
@@ -1,5 +0,0 @@
-#pragma once
-
-#include <string>
-
-std::string RecognizePhrase(const std::string& str);
