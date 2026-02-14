363687dfed1e51f7ffdc9d98217e52600c1debd2
YaShlushka <paderinee@gmail.com>
2026-02-14 11:45:15 +0300

    Обновление README.md

--- src/main.cpp
+++ src/main.cpp
@@ -3,6 +3,7 @@
 #include "voice-assistant.h"
 
 #include <boost/json.hpp>
+#include <boost/log/utility/manipulators/add_value.hpp>
 #include <boost/log/utility/setup/common_attributes.hpp>
 
 #include <atomic>
@@ -39,75 +40,83 @@ int main() {
 #if defined(_WIN32) || defined(_WIN64)
 	SetConsoleOutputCP(65001);
 #endif
-	logging::add_common_attributes();
-	auto console_sink = logging::add_console_log(
-		 std::cout, logging::keywords::format = &JsonFormatter, logging::keywords::auto_flush = true);
+	try {
+		logging::add_common_attributes();
+		auto console_sink =
+			 logging::add_console_log(std::cout, logging::keywords::format = &JsonFormatter,
+											  logging::keywords::auto_flush = true);
 
-	VoiceAssistantInit va_init;
-	std::ifstream settings_ifs("data/settings.json");
-	if (!settings_ifs) {
-		BOOST_LOG_TRIVIAL(error) << "Failed to open data/settings.json";
-		return EXIT_FAILURE;
-	}
+		VoiceAssistantInit va_init;
+		std::ifstream settings_ifs("data/settings.json");
+		if (!settings_ifs) {
+			BOOST_LOG_TRIVIAL(error) << "Failed to open data/settings.json";
+			return EXIT_FAILURE;
+		}
 
-	auto settings = json::parse(settings_ifs);
-	json::object settings_obj = settings.as_object();
-	if (settings_obj.contains("model")) {
-		va_init.model = settings_obj.at("model").as_string();
-	}
-	if (settings_obj.contains("ctx_file")) {
-		va_init.ctx_file = settings_obj.at("ctx_file").as_string();
-	}
-	if (settings_obj.contains("often_mistakes")) {
-		va_init.often_mistakes = settings_obj.at("often_mistakes").as_string();
-	}
-	if (settings_obj.contains("websites")) {
-		va_init.websites_links = settings_obj.at("websites").as_string();
-	}
-	if (settings_obj.contains("applications")) {
-		va_init.applications = settings_obj.at("applications").as_string();
-	}
-	if (settings_obj.contains("scenarios")) {
-		va_init.scenarios = settings_obj.at("scenarios").as_string();
-	}
-	if (va_init.model.empty() || va_init.ctx_file.empty()) {
-		BOOST_LOG_TRIVIAL(error) << "model of context file paths in setting.json are empty";
-		return EXIT_FAILURE;
-	}
-	va_init.stop_callback = StopProgram;
+		auto settings = json::parse(settings_ifs);
+		json::object settings_obj = settings.as_object();
+		if (settings_obj.contains("model")) {
+			va_init.model = settings_obj.at("model").as_string();
+		}
+		if (settings_obj.contains("ctx_file")) {
+			va_init.ctx_file = settings_obj.at("ctx_file").as_string();
+		}
+		if (settings_obj.contains("often_mistakes")) {
+			va_init.often_mistakes = settings_obj.at("often_mistakes").as_string();
+		}
+		if (settings_obj.contains("websites")) {
+			va_init.websites_links = settings_obj.at("websites").as_string();
+		}
+		if (settings_obj.contains("applications")) {
+			va_init.applications = settings_obj.at("applications").as_string();
+		}
+		if (settings_obj.contains("scenarios")) {
+			va_init.scenarios = settings_obj.at("scenarios").as_string();
+		}
+		if (va_init.model.empty() || va_init.ctx_file.empty()) {
+			BOOST_LOG_TRIVIAL(error) << "model of context file paths in setting.json are empty";
+			return EXIT_FAILURE;
+		}
+		va_init.stop_callback = StopProgram;
 
-	VoiceAssistant voice_assistant(va_init);
+		VoiceAssistant voice_assistant(va_init);
 
-	ma_device_config config = ma_device_config_init(ma_device_type_capture);
-	ma_device device;
+		ma_device_config config = ma_device_config_init(ma_device_type_capture);
+		ma_device device;
 
-	config.capture.format = ma_format_s16; // 16 bit
-	config.capture.channels = 1;				// mono
-	config.sampleRate = 16000;					// 16 kHz
-	config.pUserData = &voice_assistant;
-	config.dataCallback = VoiceAssistant::MiniAudioCallback;
+		config.capture.format = ma_format_s16; // 16 bit
+		config.capture.channels = 1;				// mono
+		config.sampleRate = 16000;					// 16 kHz
+		config.pUserData = &voice_assistant;
+		config.dataCallback = VoiceAssistant::MiniAudioCallback;
 
-	if (ma_device_init(NULL, &config, &device) != MA_SUCCESS) {
-		return -1;
-	}
+		if (ma_device_init(NULL, &config, &device) != MA_SUCCESS) {
+			return -1;
+		}
 
-	if (ma_device_start(&device) != MA_SUCCESS) {
+		if (ma_device_start(&device) != MA_SUCCESS) {
+			ma_device_uninit(&device);
+			return -1;
+		}
+
+		std::signal(SIGINT, SignalHandler);
+		std::signal(SIGTERM, SignalHandler);
+
+		BOOST_LOG_TRIVIAL(info) << "Program loaded successfully";
+
+		std::unique_lock<std::mutex> thread_lock(MTX);
+		CV.wait(thread_lock, [] { return STOP_REQUESTED.load(std::memory_order_relaxed); });
+
+		ma_device_stop(&device);
 		ma_device_uninit(&device);
-		return -1;
+
+		BOOST_LOG_TRIVIAL(info) << "Program was completed successfully";
+	} catch (const std::exception& ex) {
+		BOOST_LOG_TRIVIAL(error) << logging::add_value(exception, ex.what())
+										 << "Unexpected exception catched";
+		BOOST_LOG_TRIVIAL(info) << "Program was completed with errors";
+		return EXIT_FAILURE;
 	}
-
-	std::signal(SIGINT, SignalHandler);
-	std::signal(SIGTERM, SignalHandler);
-
-	BOOST_LOG_TRIVIAL(info) << "Program loaded successfully";
-
-	std::unique_lock<std::mutex> thread_lock(MTX);
-	CV.wait(thread_lock, [] { return STOP_REQUESTED.load(std::memory_order_relaxed); });
-
-	ma_device_stop(&device);
-	ma_device_uninit(&device);
-
-	BOOST_LOG_TRIVIAL(info) << "Program was completed successfully";
 
 	return EXIT_SUCCESS;
 }
